92662709b3728050b42969556e82b44f2229914c
Edward Willie <williee541@gmail.com>
2020-03-05 12:10:07 -0500

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -7,10 +7,8 @@ const stripe = Stripe('pk_test_lNFUvfSdl08p9Am7k6QjbQG500P84YN0CU');
 export const bookTour = async tourId => {
   try {
     // 1) GET CHECKOUT SESSION FROM API
-    const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
     // 2) CREATE CHECKOUT FORM + CHARGE CREDIT CARD
     await stripe.redirectToCheckout({
