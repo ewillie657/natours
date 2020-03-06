03577bcdfb3e70ddcefe8b1e4e1dfd82ce788f6a
Edward Willie <williee541@gmail.com>
2020-03-05 19:33:37 -0500

    Implement stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ import { displayMap } from './mapbox';
 import { login, logout } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -73,3 +74,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);
